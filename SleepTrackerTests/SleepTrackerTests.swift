3a5b26fe0a5dad38913fa2f4788e0ff7518dbbfb
Melissa Dennis <melissadennis120@gmail.com>
2025-04-02 10:17:23 -0400

    Add SleepTrackerTests/SleepTrackerTests.swift 1743603443202

---
+++ SleepTrackerTests/SleepTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTrackerTests.swift
+//  SleepTrackerTests
+//
+//  Created by Melissa Dennis on 31/03/2025.
+//
+
+import Testing
+@testable import SleepTracker
+
+struct SleepTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
