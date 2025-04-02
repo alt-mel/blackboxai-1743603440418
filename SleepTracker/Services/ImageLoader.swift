5576f302acd8dc37ecd34117d4f7c1eec97bc010
Melissa Dennis <melissadennis120@gmail.com>
2025-04-02 10:17:27 -0400

    Add SleepTracker/Services/ImageLoader.swift 1743603447145

---
+++ SleepTracker/Services/ImageLoader.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+final class ImageLoader {
+    static let shared = ImageLoader()
+    private var cache = NSCache<NSString, UIImage>()
+    
+    private init() {
+        // Set cache limits
+        cache.countLimit = 100
+        cache.totalCostLimit = 50 * 1024 * 1024 // 50MB
+    }
+    
+    func get(forKey key: String) -> UIImage? {
+        return cache.object(forKey: key as NSString)
+    }
+    
+    func set(_ image: UIImage, forKey key: String) {
+        cache.setObject(image, forKey: key as NSString)
+    }
+    
+    func clear() {
+        cache.removeAllObjects()
+    }
+}
